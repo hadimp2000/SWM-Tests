6dbee57b6a3839587a33724b087f2e39196ee00b
hadi <hmp123475@gmail.com>
2024-11-22 11:10:29 +0100

    HP-03 completed.

--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -1,29 +1,36 @@
 describe("Homepage Tests", () => {
-    beforeEach(() => {
-      cy.visit("/");
-    });
-    it("HP-01: Verify homepage loads correctly", () => {
-      cy.get('[data-testid="banner-headline"]')
-        .should("be.visible")
-        .and("contain.text", "SysBag WP");
-  
-      cy.get('[data-testid="page-heading-headline"]')
-        .should("be.visible")
-        .and("contain.text", "Discover");
-    });
-  
-    it("HP-02: Verify search bar functionality", () => {
-      cy.get('input[placeholder="Search"]') // Adjust the selector if needed
-        .should("be.visible") // Ensure the search bar is visible
-        .type("SysBag WP"); // Replace with the product name you want to test
-  
-      // Step 2: Click the "Search" button
-      // cy.get('button[type="submit"]').click(); // Adjust the selector for the Search button
-  
-      // // Step 3: Verify that relevant products appear
-      // cy.get(".search-results") // Replace with the selector for the search results container
-      //   .should("be.visible") // Ensure results are displayed
-      //   .and("contain.text", "SysBag WP"); // Verify the product name is in the results
-    });
+  beforeEach(() => {
+    cy.visit("/");
   });
-  
+  it("HP-01: Verify homepage loads correctly", () => {
+    cy.get('[data-testid="banner-headline"]')
+      .should("be.visible")
+      .and("contain.text", "SysBag WP");
+
+    cy.get('[data-testid="page-heading-headline"]')
+      .should("be.visible")
+      .and("contain.text", "Discover");
+  });
+
+  it("HP-02: Verify search bar functionality", () => {
+    cy.get('input[placeholder="Search"]').click().type("SysBag WP{enter}");
+  });
+
+  it("HP-03: Verify category navigation", () => {
+    cy.get('button[data-variant="secondary"]')
+      .first()
+      .should("be.visible")
+      .click();
+
+    cy.get("div.typo-xs_list_base")
+      .find("div.flex-1")
+      .contains("Protection")
+      .should("be.visible")
+      .click();
+
+    cy.get("a")
+      .contains(/go to protection/i)
+      .should("be.visible")
+      .click();
+  });
+});
